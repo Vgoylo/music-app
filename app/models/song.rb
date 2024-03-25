1784996fd60d734bd3bcd086a8f1287f1f9bd285
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-25 14:12:21 +0300

    Change scope in model song rename days on period

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,12 @@
 class Song < ApplicationRecord
-  self.implicit_order_column = 'title'
-
   belongs_to :artist
   has_many :downloads, dependent: :destroy
 
   validates :length, numericality: { greater_than: 5 }
   validates :filesize, numericality: { greater_than: 0 }
 
-  scope :top, ->(days, count) { joins("INNER JOIN (SELECT COUNT(*) as downloads_count, song_id FROM downloads
-    WHERE downloads.created_at > '#{days.public_send(:days).ago}' GROUP BY song_id) as downloads ON songs.id = downloads.song_id")
+  scope :top, ->(period, count) { joins("INNER JOIN (SELECT COUNT(*) as downloads_count, song_id FROM downloads
+    WHERE downloads.created_at > '#{period.ago}' GROUP BY song_id) as downloads ON songs.id = downloads.song_id")
     .order('downloads.downloads_count DESC').limit(count)}
 
 end
