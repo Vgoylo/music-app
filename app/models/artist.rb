d8426f04e92324c4dd0e0a3bf956619b10569c82
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-25 16:57:39 +0300

    Changes scope in models artist on <<-SQL.squish

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,16 +5,27 @@ class Artist < ApplicationRecord
 
   scope :top, ->(letter, count) {
     select('artists.*, SUM(downloads.downloads_count) as songs_downloads')
-    .joins("INNER JOIN songs ON artists.id = songs.artist_id
-          INNER JOIN (SELECT COUNT(*) as downloads_count, song_id FROM downloads GROUP BY song_id) as downloads
-          ON songs.id = downloads.song_id")
+    .joins(
+      <<-SQL.squish
+        JOIN songs ON artists.id = songs.artist_id
+        JOIN (
+          SELECT COUNT(*) as downloads_count, song_id FROM downloads
+          GROUP BY song_id
+        ) AS downloads ON songs.id = downloads.song_id
+      SQL
+    )
     .where("name like :letter", letter: "#{letter}%").group(:id)
     .order('songs_downloads DESC').limit(count)
     }
 
   def songs_top
-    songs.joins("INNER JOIN (SELECT COUNT(*) as downloads_count, song_id FROM downloads
-    GROUP BY song_id) as downloads ON songs.id = downloads.song_id")
-    .order('downloads.downloads_count DESC')
+    songs.joins(
+      <<-SQL.squish
+        JOIN (
+          SELECT COUNT(*) as downloads_count, song_id FROM downloads
+          GROUP BY song_id
+        ) AS downloads ON songs.id = downloads.song_id
+      SQL
+    ).order('downloads.downloads_count DESC')
   end
 end
