8c22767e2dcfe39803063897be857836dcf8933c
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-25 17:01:27 +0300

    Add callback method in model download

--- app/models/download.rb
+++ app/models/download.rb
@@ -1,3 +1,11 @@
 class Download < ApplicationRecord
-  belongs_to :song 
+  belongs_to :song
+
+  before_create :set_date
+
+  private
+
+  def set_date
+    self.created_date = rand(Time.at(2.months.ago)..Time.at(Time.now))
+  end
 end
