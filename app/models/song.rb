1d4cf03e895c2e747ceeab509754e86c20f8e6d7
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-25 15:04:37 +0300

    Refactoring request  sql  for on db  in model song scope

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,9 +4,13 @@ class Song < ApplicationRecord
 
   validates :length, numericality: { greater_than: 5 }
   validates :filesize, numericality: { greater_than: 0 }
-
-  scope :top, ->(period, count) { joins("INNER JOIN (SELECT COUNT(*) as downloads_count, song_id FROM downloads
-    WHERE downloads.created_at > '#{period.ago}' GROUP BY song_id) as downloads ON songs.id = downloads.song_id")
-    .order('downloads.downloads_count DESC').limit(count)}
-
+  
+  scope :top, ->(period, count) { joins(
+        <<-SQL.squish
+          JOIN (
+            SELECT COUNT(*) as downloads_count, song_id FROM downloads
+            WHERE downloads.created_at > '#{period.ago}' GROUP BY song_id) AS downloads
+            ON songs.id = downloads.song_id
+        SQL
+    ).order('downloads_count DESC').limit(count) }
 end
